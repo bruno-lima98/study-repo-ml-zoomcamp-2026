68b829bf8949a73c1939283d82e18056b7890bb1
Bruno Lima <brunolimawp@gmail.com>
2026-09-24 08:21:48 -0300

    class 04 - commit 2

--- classes/class_04.py
+++ classes/class_04.py
@@ -4,9 +4,12 @@ import numpy as np
 from IPython.display import display
 import random
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
 
 from sklearn import model_selection
-from sklearn.metrics import mutual_info_score, accuracy_score, confusion_matrix, precision_score, recall_score
+from sklearn.metrics import mutual_info_score, accuracy_score, confusion_matrix
+from sklearn.metrics import precision_score, recall_score, roc_curve, roc_auc_score
+from sklearn.metrics import average_precision_score, precision_recall_curve
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
 
@@ -106,4 +109,96 @@ confusion_matrix(y_val, y_pred_final)
 precision_score(y_val, y_pred_final)
 
 # %%
-recall_score (y_val, y_pred_final)
+recall_score(y_val, y_pred_final)
+
+# %%
+fpr, tpr, thresholds = roc_curve(y_val, y_pred)
+auc = roc_auc_score(y_val, y_pred)
+
+plt.figure(figsize=(5,5))
+
+plt.plot(fpr, tpr, label=f"AUC = {auc:.3f}")
+plt.plot([0,1], [0,1], color="black", linestyle="dashed")
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("ROC Curve")
+plt.legend()
+plt.savefig("images/class_04_01_roc_curve")
+
+plt.show()
+
+# %%
+precision, recall, thresholds = precision_recall_curve(y_val, y_pred)
+ap = average_precision_score(y_val, y_pred)
+
+plt.figure(figsize=(5,5))
+
+plt.plot(recall, precision, label=f"AP = {ap:.3f}")
+plt.xlabel("Recall")
+plt.ylabel("Precision")
+plt.title("Precision-Recall Curve")
+plt.legend()
+plt.savefig("images/class_04_02_precision_recall_curve")
+
+plt.show()
+
+# %%
+def train(df_train, y_train, C=1.0):
+    dicts = df_train[categorical_columns + numerical_columns].to_dict(orient="records")
+ 
+    dv = DictVectorizer(sparse=False)
+    X_train = dv.fit_transform(dicts)
+
+    model = LogisticRegression(C=C, max_iter=100000)
+    model.fit(X_train, y_train)
+
+    return dv, model
+
+df, model = train(df_train, y_train, C=000.1)
+
+# %%
+def predict(df, dv, model):
+    dicts = df[categorical_columns + numerical_columns].to_dict(orient="records")
+
+    X = dv.transform(dicts)
+    y_pred = model.predict_proba(X)[:, 1]
+
+    return y_pred
+
+y_pred = predict(df_val, dv, model)
+
+# %%
+kfold = model_selection.KFold(n_splits=10, shuffle=True, random_state=42)
+
+# %%
+n_splits = 10
+C = [0.0001, 0.001, 0.01, 0.1, 0.5, 1, 5, 10]
+
+for c in C:
+    scores = []
+
+    kfold = model_selection.KFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+    for train_idx, val_idx in tqdm(kfold.split(df_full_train), total=n_splits):
+        df_train = df_full_train.iloc[train_idx]
+        df_val = df_full_train.iloc[val_idx]
+
+        y_train = df_train["churn"].values
+        y_val = df_val["churn"].values
+
+        dv, model = train(df_train, y_train, c)
+        y_pred = predict(df_val, dv, model)
+
+        auc = roc_auc_score(y_val, y_pred)
+        scores.append(auc)
+
+    print(f"C = {c} | {np.mean(scores).round(4)} +- {np.std(scores).round(4)}")
+
+# %%
+dv, model = train(df_full_train, y_full_train, C = 10)
+y_pred = predict(df_test, dv, model)
+
+auc = roc_auc_score(y_test, y_pred)
+auc
+
+# %%
